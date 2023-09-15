c30a6b9bea67faf5f509a0fbd75f35e13d4dd927
npgiano-macbook <npgiano@gmail.com>
2023-09-15 01:33:59 -0400

    Deploy Fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
             instances: 'max',
             script: './.output/server/index.mjs',
             env_production: {
+                NITRO_PORT: '3333',
                 DATABASE_URL: process.env.DATABASE_URL
             }
         }
